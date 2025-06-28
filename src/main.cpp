d76b958401c60955466dd5ccec2c6543b5e8083c
rferrell03 <referrelliii@gmail.com>
2025-06-28 15:48:38 -0500

    added linear algebra (transformations)

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
 #include <iostream>
 #include "shader.h"
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -26,7 +32,7 @@ int main() {
 	////////////////////
 
 	//Creates window
-	GLFWwindow* window = glfwCreateWindow(800, 600, "oh my god a burger", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1200, 600, "probably an epic graphical render", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -42,7 +48,7 @@ int main() {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, 1200, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	/////////////////--- END WINDOW SETUP ---////////////////////////
 
@@ -52,26 +58,54 @@ int main() {
 
 	//Vertex input
 	float vertices[] = {
-		// pos         // color           // tex coords
-		-0.8f, -0.8f, 0.0f, 0.8f, 0.0f, 0.0f, 0.0f, 0.0f, // bottom-left
-		 0.8f, -0.8f, 0.0f, 0.0f, 0.8f, 0.0f, 1.0f, 0.0f, // bottom-right
-		 0.8f,  0.8f, 0.0f, 0.0f, 0.0f, 0.8f, 1.0f, 1.0f, // top-right
-		-0.8f,  0.8f, 0.0f, 0.8f, 0.8f, 0.8f, 0.0f, 1.0f  // top-left
+		// Format: x, y, z, r, g, b, u, v
+		-0.8f, -0.8f, 0.0f, 1, 0, 0, 0.0f, 0.0f,
+		 0.8f, -0.8f, 0.0f, 0, 1, 0, 1.0f, 0.0f,
+
+		-0.8f, -0.64f, 0.0f, 1, 0, 0, 0.0f, 0.1f,
+		 0.8f, -0.64f, 0.0f, 0, 1, 0, 1.0f, 0.1f,
+
+		-0.8f, -0.48f, 0.0f, 1, 0, 0, 0.0f, 0.2f,
+		 0.8f, -0.48f, 0.0f, 0, 1, 0, 1.0f, 0.2f,
+
+		-0.8f, -0.32f, 0.0f, 1, 0, 0, 0.0f, 0.3f,
+		 0.8f, -0.32f, 0.0f, 0, 1, 0, 1.0f, 0.3f,
+
+		-0.8f, -0.16f, 0.0f, 1, 0, 0, 0.0f, 0.4f,
+		 0.8f, -0.16f, 0.0f, 0, 1, 0, 1.0f, 0.4f,
+
+		-0.8f,  0.0f,  0.0f, 1, 0, 0, 0.0f, 0.5f,
+		 0.8f,  0.0f,  0.0f, 0, 1, 0, 1.0f, 0.5f,
+
+		-0.8f,  0.16f, 0.0f, 1, 0, 0, 0.0f, 0.6f,
+		 0.8f,  0.16f, 0.0f, 0, 1, 0, 1.0f, 0.6f,
+
+		-0.8f,  0.32f, 0.0f, 1, 0, 0, 0.0f, 0.7f,
+		 0.8f,  0.32f, 0.0f, 0, 1, 0, 1.0f, 0.7f,
+
+		-0.8f,  0.48f, 0.0f, 1, 0, 0, 0.0f, 0.8f,
+		 0.8f,  0.48f, 0.0f, 0, 1, 0, 1.0f, 0.8f,
+
+		-0.8f,  0.64f, 0.0f, 1, 0, 0, 0.0f, 0.9f,
+		 0.8f,  0.64f, 0.0f, 0, 1, 0, 1.0f, 0.9f,
+
+		-0.8f,  0.8f,  0.0f, 1, 0, 0, 0.0f, 1.0f,
+		 0.8f,  0.8f,  0.0f, 0, 1, 0, 1.0f, 1.0f
 	};
 
-	unsigned int indices[] = {
+	/*unsigned int indices[] = {
 		0, 1, 2,
 		2, 3, 0
-	};
+	};*/
 	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	//glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -86,6 +120,8 @@ int main() {
 	//Set up shader
 
 	Shader myShader(vertexShaderSource, fragShaderSource);
+	GLint timeLoc = glGetUniformLocation(myShader.ID, "time");
+	//Transform stuff
 
 	//set up texture
 	unsigned int texture;
@@ -98,7 +134,7 @@ int main() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	// load and generate the texture
 	int width, height, nrChannels;
-	unsigned char* data = stbi_load("textures/burger.jpg", &width, &height, &nrChannels, 0);
+	unsigned char* data = stbi_load("textures/aaa.jpg", &width, &height, &nrChannels, 0);
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -120,15 +156,29 @@ int main() {
 		// input
 		processInput(window);
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.3, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
-
+		float currentTime = (float)glfwGetTime();
 		myShader.use();
-		glBindTexture(GL_TEXTURE_2D, texture);
+		glUniform1f(timeLoc, currentTime);
 
+
+		//Transformation calculation
+		glm::mat4 trans = glm::mat4(1.0f);
+		trans = glm::rotate(trans, currentTime, glm::vec3(0.0, 0.0, 5.0));
+		trans = glm::scale(trans, glm::vec3(0.5, (sin(currentTime)+1)/2, 0.5));
+		myShader.setMat4("transform", trans);
+
+
+
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		for (int i = 0; i < 10; ++i) {
+			glUniform1f(timeLoc, currentTime - i * 0.1f);
+			glDrawArrays(GL_TRIANGLE_STRIP, 0, 22);
+		}
+
 		
 
 		// check and call events and swap the buffers
