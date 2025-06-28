2ef82e42e430e9ab4b53f433fd58e37f6e654c76
rferrell03 <referrelliii@gmail.com>
2025-06-28 18:08:08 -0500

    3d objects!!! MVP matricies set up and sample cubes are rendered. huge success imo

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/string_cast.hpp>
 
+#include <random>
 #include <iostream>
 #include "shader.h"
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -48,6 +49,8 @@ int main() {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+
+	glEnable(GL_DEPTH_TEST);
 	glViewport(0, 0, 1200, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	/////////////////--- END WINDOW SETUP ---////////////////////////
@@ -58,40 +61,55 @@ int main() {
 
 	//Vertex input
 	float vertices[] = {
-		// Format: x, y, z, r, g, b, u, v
-		-0.8f, -0.8f, 0.0f, 1, 0, 0, 0.0f, 0.0f,
-		 0.8f, -0.8f, 0.0f, 0, 1, 0, 1.0f, 0.0f,
+		// Back face
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-		-0.8f, -0.64f, 0.0f, 1, 0, 0, 0.0f, 0.1f,
-		 0.8f, -0.64f, 0.0f, 0, 1, 0, 1.0f, 0.1f,
+		// Front face
+		-0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
 
-		-0.8f, -0.48f, 0.0f, 1, 0, 0, 0.0f, 0.2f,
-		 0.8f, -0.48f, 0.0f, 0, 1, 0, 1.0f, 0.2f,
+		// Left face
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
-		-0.8f, -0.32f, 0.0f, 1, 0, 0, 0.0f, 0.3f,
-		 0.8f, -0.32f, 0.0f, 0, 1, 0, 1.0f, 0.3f,
+		// Right face (FIXED)
+		 0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-		-0.8f, -0.16f, 0.0f, 1, 0, 0, 0.0f, 0.4f,
-		 0.8f, -0.16f, 0.0f, 0, 1, 0, 1.0f, 0.4f,
+		 // Bottom face
+		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		  0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
 
-		-0.8f,  0.0f,  0.0f, 1, 0, 0, 0.0f, 0.5f,
-		 0.8f,  0.0f,  0.0f, 0, 1, 0, 1.0f, 0.5f,
-
-		-0.8f,  0.16f, 0.0f, 1, 0, 0, 0.0f, 0.6f,
-		 0.8f,  0.16f, 0.0f, 0, 1, 0, 1.0f, 0.6f,
-
-		-0.8f,  0.32f, 0.0f, 1, 0, 0, 0.0f, 0.7f,
-		 0.8f,  0.32f, 0.0f, 0, 1, 0, 1.0f, 0.7f,
-
-		-0.8f,  0.48f, 0.0f, 1, 0, 0, 0.0f, 0.8f,
-		 0.8f,  0.48f, 0.0f, 0, 1, 0, 1.0f, 0.8f,
-
-		-0.8f,  0.64f, 0.0f, 1, 0, 0, 0.0f, 0.9f,
-		 0.8f,  0.64f, 0.0f, 0, 1, 0, 1.0f, 0.9f,
-
-		-0.8f,  0.8f,  0.0f, 1, 0, 0, 0.0f, 1.0f,
-		 0.8f,  0.8f,  0.0f, 0, 1, 0, 1.0f, 1.0f
+		 // Top face
+		 -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	  	  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	  	 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
 	};
+
 
 	/*unsigned int indices[] = {
 		0, 1, 2,
@@ -107,12 +125,10 @@ int main() {
 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
@@ -146,10 +162,33 @@ int main() {
 	}
 	stbi_image_free(data);
 
+
+
 	//wireframe
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	int count = 0;
+	//gen cubes
+	int count = 40;
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+	std::uniform_real_distribution<float> speed(1.0f, 3.0f);
+	std::uniform_real_distribution<float> speedDist(0.8f, 2.0f);
+	std::uniform_real_distribution<float> distX(-3.5f, 3.5f);
+	std::uniform_real_distribution<float> distY(-3.0f, 3.0f);
+	std::uniform_real_distribution<float> distZ(-5.5f, -4.5f);
+
+	std::vector<glm::vec3> cubePositions = {};
+	std::vector<glm::vec3> cubeRotations;
+	std::vector<float> cubeSpeeds;
+
+	
+	for (int i = 0; i < count; ++i) {
+		cubeSpeeds.push_back(speedDist(gen));
+		cubeRotations.emplace_back(dist(gen), dist(gen), dist(gen) );
+		cubePositions.emplace_back(distX(gen), distY(gen), distZ(gen));
+	}
+
 	//render loop!
 	while (!glfwWindowShouldClose(window))
 	{
@@ -157,26 +196,40 @@ int main() {
 		processInput(window);
 
 		glClearColor(0.3, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		float currentTime = (float)glfwGetTime();
 		myShader.use();
 		glUniform1f(timeLoc, currentTime);
 
+		//MVP
+		glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
+		//model = glm::rotate(model, currentTime / 2 * glm::radians(-55.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		//model = glm::rotate(model, currentTime * glm::radians(180.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		//model = glm::rotate(model, currentTime * glm::radians(90.0f), glm::vec3(0.0f, 0.5f, 0.0f));
+
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		projection = glm::perspective(glm::radians(45.0f), (float)1200 / (float)600, 0.1f, 100.0f);
 
 		//Transformation calculation
-		glm::mat4 trans = glm::mat4(1.0f);
-		trans = glm::rotate(trans, currentTime, glm::vec3(0.0, 0.0, 5.0));
-		trans = glm::scale(trans, glm::vec3(0.5, (sin(currentTime)+1)/2, 0.5));
-		myShader.setMat4("transform", trans);
+
+		myShader.setMat4("view", view);
+		myShader.setMat4("projection", projection);
 
 
 
 		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
-		for (int i = 0; i < 10; ++i) {
+		for (int i = 0; i < count; ++i) {
 			glUniform1f(timeLoc, currentTime - i * 0.1f);
-			glDrawArrays(GL_TRIANGLE_STRIP, 0, 22);
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			model = glm::scale(model, glm::vec3(0.7));
+			model = glm::rotate(model, sin(currentTime * cubeSpeeds[i]) * glm::radians(180.0f), cubeRotations[i]);
+			myShader.setMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
 
 		
